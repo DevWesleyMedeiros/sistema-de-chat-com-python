811806c6bd85cc6bb1025f5ce1cf5ee84c108751
Wesley Medeiros <da.rosa.wesley25@gmail.com>
2023-09-29 16:15:53 -0300

    projeto finalizado

--- codigo.py
+++ codigo.py
@@ -3,15 +3,41 @@ import flet as ft
 
 def main(pagina):
     
+    chat = ft.Column()
+    
+    def enviar_mensagem_tunel(mensagem):
+        tipo = mensagem["tipo"]
+        if tipo == "mensagem":
+            texto_mensagem = mensagem["Texto"]
+            usuario_mensagem = mensagem["Usuário"]
+            chat.controls.append(ft.Text(f"{usuario_mensagem} : {texto_mensagem}"))
+        else:
+            usuario_mensagem = mensagem["Usuário"]
+            chat.controls.append(ft.Text(f"{usuario_mensagem} entrou no site", size=20, italic=True, color=ft.colors.ORANGE_600))
+        pagina.update()
+        
+    
+    pagina.pubsub.subscribe(enviar_mensagem_tunel)
+    
+    
+    def enviar_mensagem(evento):
+        pagina.pubsub.send_all({"Texto": campo_mensagem.value, "Usuário": nome_usuario.value, "tipo":"mensagem"})
+        campo_mensagem.value = ""
+        pagina.update()
+    
     nome_usuario = ft.TextField(label="Escreva o seu nome")
-    campo_mensagem = ft.TextField(label="Digite uma mensagem")
-    botao_enviar_mensagem = ft.ElevatedButton("Enviar")
+    campo_mensagem = ft.TextField(label="Digite uma mensagem", on_submit=enviar_mensagem)
+    botao_enviar_mensagem = ft.ElevatedButton("Enviar", on_click=enviar_mensagem)
     
     def entrar_popup(evento):
+        pagina.pubsub.send_all({"Usuário": nome_usuario.value, "tipo":"entrada"})
         popup.open = False
+        pagina.add(chat)
         pagina.remove(botao_iniciar)
-        pagina.add(campo_mensagem)
-        pagina.add(botao_enviar_mensagem)
+        pagina.remove(texto)
+        pagina.add(ft.Row(
+            [campo_mensagem, botao_enviar_mensagem]
+        ))
         pagina.update()
         
     popup = ft.AlertDialog(
@@ -33,4 +59,4 @@ def main(pagina):
     pagina.add(texto)
     pagina.add(botao_iniciar)
     
-ft.app(target=main)
+ft.app(target=main, view=ft.WEB_BROWSER, port=8000)
